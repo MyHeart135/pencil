d60097ffa12198d7a8dc3ba0f510f856b888262a
yanhuan1 <yanhuan1@jd.com>
2018-04-22 11:47:50 +0800

    测试类

---
+++ pencil-business/src/test/java/com/yanhuan/BaseTest.java
@@ -0,0 +1,14 @@
+package java.com.yanhuan;
+
+import org.junit.Test;
+
+/**
+ * 测试搭建成功
+ */
+public class BaseTest {
+
+    @Test
+    public void test(){
+        System.out.println("Success!");
+    }
+}
